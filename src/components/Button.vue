7be7db856e2b94db1746ce4c020ef8231b22893d
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-06 22:53:19 +0800

    g-Button

---
+++ src/components/Button.vue
@@ -0,0 +1,34 @@
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: {
+                type: String,
+                default: 'This is a button'
+            },
+            color: {
+                type: String,
+                default: 'green'
+            }
+        }
+    }
+</script>
+
+<template>
+    <button
+        @click="$emit('btn-click')"
+        :style="{background: color}"
+    >
+        {{ text }}
+    </button>
+</template>
+
+<style scoped lang="scss">
+    button {
+        outline: 0;
+        border: none;
+        padding: 12px 16px;
+        color: white;
+        border-radius: 4px;
+    }
+</style>
