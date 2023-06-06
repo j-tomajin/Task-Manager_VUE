0e430f24e9a433d424c0657274b979d6469bad6d
Jerickson Tomajin <tomajinj@gmail.com>
2023-06-06 22:53:36 +0800

    Tasks

---
+++ src/components/Tasks.vue
@@ -0,0 +1,34 @@
+<template>
+    <div
+        class="task"
+        v-for="task in tasks"
+        :key="task.id"
+    >
+        <Task 
+            @delete-task="$emit('delete-task', task.id)"
+            @toggle-reminder="$emit('toggle-reminder', task.id)"
+            :task="task" />
+    </div>
+    <p>Nothing follows</p>
+</template>
+
+<script>
+    import Task from './Task.vue';
+
+    export default {
+        name: 'Tasks',
+        props: {
+            tasks: Array,
+        },
+        components: {
+            Task,
+        },
+        emits: ['delete-task', 'toggle-reminder']
+    }
+</script>
+
+<style lang="scss" scoped>
+    p {
+        text-align: center;
+    }
+</style>
